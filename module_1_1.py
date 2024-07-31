f34342f85f5b673f5cfc20dd9cee83760198135c
Grikh-UU-Py <5186415@mail.ru>
2024-07-31 12:33:16 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,6 @@
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:]) #тут наверно надо как-то посчитать общее кол-во символов и поделить на 2
+print(example[::-1])
+print(example[1::2])
